ec687505b8cad155f5c95586b0ca775881713616
beomi <latheledusjp@gmail.com>
2017-12-05 14:59:45 +0900

    Add naver asset

--- 02_naver_realasset_nav.py
+++ 02_naver_realasset_nav.py
@@ -1,2 +1,16 @@
 # 네이버 부동산 '글자' 가져오기
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+req = requests.get('http://naver.com')
+html = req.text
+
+soup = bs(html, 'lxml')
+
+부동산 = soup.select_one(
+    '#PM_ID_serviceNavi > li:nth-of-type(4) > a > span.an_txt'
+)
+
+print(부동산.text)
 
